5c12c9267698eed34c5d5a6b0d5db1e21c67bc72
Jared Perlmutter <jperlmutter1@gmail.com>
2024-10-15 22:53:32 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-
+# Set up Chrome options
 chrome_options = Options()
 options = [
-    "--headless",
+    "--headless",  # Run headlessly
     "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
@@ -19,7 +17,17 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
+# Set up the Chrome driver service
+chrome_service = Service(ChromeDriverManager().install())
+
+# Initialize the WebDriver
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
+# Navigate to a URL
 driver.get('http://nytimes.com')
+
+# Print the page title
 print(driver.title)
+
+# Close the driver
+driver.quit()
