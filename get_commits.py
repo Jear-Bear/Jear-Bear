460954302edea71ac81f7208683cdba211994ef0
Jared Perlmutter <jperlmutter1@gmail.com>
2024-10-15 19:23:08 -0500

    Update get_commits.py

--- get_commits.py
+++ get_commits.py
@@ -10,5 +10,6 @@ soup = BeautifulSoup(response.text, 'html.parser')
 total_commits = soup.find('span', class_='text-bold color-fg-default').text.strip()
 
 # Write the total commits to commits.md
-with open('commits.md', 'w') as file:
-    file.write(f'# Total Commits\n\nTotal Commits: {total_commits}\n')
+with open("total_commits.txt", "w") as f:
+    f.write(total_commits)
+    print("File written to total_commits.txt")
