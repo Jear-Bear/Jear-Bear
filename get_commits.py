ff9e4b8291a4936864439e67dd8300d81e3c805e
Jared Perlmutter <jperlmutter1@gmail.com>
2024-10-15 20:23:24 -0500

    Update get_commits.py

--- get_commits.py
+++ get_commits.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
+chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
 # Set up the Chrome driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
 
 # URL of the GitHub profile
 url = "https://github.com/Jear-Bear"
