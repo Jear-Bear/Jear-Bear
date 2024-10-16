2568c3e1c36fca9ea73e3b649582a07f43f28201
Jared Perlmutter <jperlmutter1@gmail.com>
2024-10-15 22:48:37 -0500

    Update get_commits.py

--- get_commits.py
+++ get_commits.py
@@ -1,26 +1,25 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-import time
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-# Set up the Chrome driver
-driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-# Navigate to your GitHub contributions page
-driver.get("https://github.com/Jear-Bear")
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
 
-# Wait for the page to load
-time.sleep(5)
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-# Extract the HTML content
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-# Find the relevant information you want to scrape (example: total contributions)
-# Adjust the selector based on the actual structure of your profile page
-commit_count = soup.find('h2', class_='f4 text-normal mb-2').get_text(strip=True)
-
-# Print the total commits count to the terminal
-print("Total Commits:", commit_count)
-
-# Close the driver
-driver.quit()
+driver.get('https://github.com/Jear-Bear')
+print(driver.title)
